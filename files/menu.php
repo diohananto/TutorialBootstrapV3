d98bc9ccb4c1753ac1bc496c041d67af069e0364
diohananto <dionrangga98@gmail.com>
2018-05-22 10:25:31 +0700

    membuat menu website

--- files/menu.php
+++ files/menu.php
@@ -2,6 +2,11 @@
     <div class="clearfix">
         <div class="container">            
             <h3>Pertemuan 01</h3>
+            <ul class="nav nav-tabs">                
+                <li class="active"><a href="#">Menu 1</a></li>
+                <li><a href="#">Menu 2</a></li>
+                <li><a href="#">Menu 3</a></li>
+            </ul>            
         </div>
     </div>
 </nav>
